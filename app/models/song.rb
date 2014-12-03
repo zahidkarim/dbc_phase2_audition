43597757314e9ae0ab991d5b4a1e7a438f6e06fa
ZahidKarim <zahid.jethani@gmail.com>
2014-12-03 09:14:37 -0500

    Created associations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
+  belongs_to :playlist
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
+  #auth
   validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: true
   has_secure_password
+  #app
+  has_many :playlists
+  has_many :songs, through: :playlists
 
 end
