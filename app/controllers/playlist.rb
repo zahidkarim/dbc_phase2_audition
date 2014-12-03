a6c14e494303e05c9ee045846717a40fe2b96744
ZahidKarim <zahid.jethani@gmail.com>
2014-12-03 10:33:07 -0500

    Added my playlists route and erb, also created links in nav bar for the route

--- app/controllers/playlist.rb
+++ app/controllers/playlist.rb
@@ -6,6 +6,12 @@ get '/playlists/all' do
     erb :"/playlist/all"
 end
 
+get '/my_playlists' do
+  logged_in
+    user = current_user
+    @playlists = user.playlists
+    erb :"/playlist/my_playlists"
+end
 #View one playlist - all the songs in it
 get '/playlists/:id' do |id|
   logged_in
