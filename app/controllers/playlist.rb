10159b030a16cabbdb5abcbe980a1f3e05e93038
ZahidKarim <zahid.jethani@gmail.com>
2014-12-03 10:41:35 -0500

    FIxed nav_bar so all links work and added a my_songs page

--- app/controllers/playlist.rb
+++ app/controllers/playlist.rb
@@ -6,12 +6,14 @@ get '/playlists/all' do
     erb :"/playlist/all"
 end
 
+#View all my playlists
 get '/my_playlists' do
   logged_in
     user = current_user
     @playlists = user.playlists
     erb :"/playlist/my_playlists"
 end
+
 #View one playlist - all the songs in it
 get '/playlists/:id' do |id|
   logged_in
--- app/controllers/song.rb
+++ app/controllers/song.rb
@@ -1,3 +1,11 @@
+#view all my songs
+get '/songs/my_songs' do
+  logged_in
+  user = current_user
+  @songs = user.songs
+  erb :"/song/my_songs"
+end
+
 #View one song
 get '/songs/:id' do |id|
   logged_in
