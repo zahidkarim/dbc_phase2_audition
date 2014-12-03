dcc7bf8b55415bab5e6209c625e119c54ac837b3
ZahidKarim <zahid.jethani@gmail.com>
2014-12-03 09:10:30 -0500

    Filled in migrations

--- db/migrate/20141203090436_create_playlists.rb
+++ db/migrate/20141203090436_create_playlists.rb
@@ -1,4 +1,10 @@
 class CreatePlaylists < ActiveRecord::Migration
   def change
+    create_table :playlists do |t|
+      t.string :name
+      t.references :user
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20141203090442_create_songs.rb
+++ db/migrate/20141203090442_create_songs.rb
@@ -1,4 +1,12 @@
 class CreateSongs < ActiveRecord::Migration
   def change
+    create_table :songs do |t|
+      t.string      :title
+      t.string      :song_url
+      t.references  :user
+      t.references  :playlist
+
+      t.timestamps
+    end
   end
 end
