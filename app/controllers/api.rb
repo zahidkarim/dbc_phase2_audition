8b783ecee8445bc7441318f693188aa104eb3d6d
ZahidKarim <zahid.jethani@gmail.com>
2014-12-03 19:49:47 -0500

    Burnt out...tired...can't get streaming to work...

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -2,3 +2,10 @@ get '/soundcloud' do
 @tracks = CLIENT.get('/tracks', :q => params[:search], :licence => 'cc-by-sa')
 erb :'/api/soundcloud'
 end
+
+#Try and get streaming working
+# get '/soundcloud1' do
+# track = CLIENT.get('/tracks/293')
+# @stream_url = CLIENT.get(track.stream_url, :allow_redirects => true)
+# erb :'/api/soundcloud1'
+# end
